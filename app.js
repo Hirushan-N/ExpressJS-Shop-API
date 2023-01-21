7c6831bf45882227f61d64d4c124c89cffca15a0
Hirushan-N <nadeeshhirushan@gmail.com>
2023-01-21 14:53:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 
 const productRoutes = require('./api/routes/productRoutes')
+const orderRoutes = require('./api/routes/orderRoutes')
 
 app.use('/products',productRoutes);
+app.use('/orders',orderRoutes);
+
 
 module.exports = app;
