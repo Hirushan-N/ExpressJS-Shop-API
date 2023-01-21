05271e2ae9a58d60cff9d22db3db066de656ddd1
Hirushan-N <nadeeshhirushan@gmail.com>
2023-01-21 14:52:53 +0530

    Update productRoutes.js

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -4,8 +4,44 @@ const router = express.Router();
 router.get('/',(req,res,next) => {
     res.status(200).json({
         message : "Get all products"
-    })
-})
+    });
+});
+
+router.post('/',(req,res,next) => {
+    res.status(200).json({
+        message : "POST product"
+    });
+});
+
+router.get('/:productId',(req,res,next) => {
+    const Id = req.params.productId;
+    res.status(200).json({
+        message : "GET product by productId",
+        content : {
+            productId : Id
+        }
+    });
+});
+
+router.patch('/:productId',(req,res,next) => {
+    const Id = req.params.productId;
+    res.status(200).json({
+        message : "Update product by productId",
+        content : {
+            productId : Id
+        }
+    });
+});
+
+router.delete('/:productId',(req,res,next) => {
+    const Id = req.params.productId;
+    res.status(200).json({
+        message : "DELETE product by productId",
+        content : {
+            productId : Id
+        }
+    });
+});
 
 
 module.exports = router;
