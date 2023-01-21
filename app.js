95cc5abe323484fe403e05e1aab4717884dcf99f
Hirushan-N <nadeeshhirushan@gmail.com>
2023-01-21 17:13:45 +0530

    added CORS error handlers

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express'); //npm install --save express //npm install --save-dev nodemon
 const app = express();
 const morgan = require('morgan'); //npm install --save morgan
+const bodyParser = require('body-parser') //npm install --save body-parser
 
 const productRoutes = require('./api/routes/productRoutes')
 const orderRoutes = require('./api/routes/orderRoutes');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use((req,res,next) => {
+    res.header(
+        "Access-Control-Allow-Origin",
+        "*"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested_With,Content_type,Accept,Authorization"
+    );
+    if(req.method === 'PRTIONS'){
+        res.header(
+            "Access-Control-Allow-Methods",
+            "PUT,POST,PATCH.DELETE,GET"
+        );
+        return res.status(200).json({});
+    }
+    next();
+}); 
 
 //routes
 app.use('/products',productRoutes);
