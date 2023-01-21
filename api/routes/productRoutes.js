d52268f2a891b17bf030243cc7b6f607d7a165df
Hirushan-N <nadeeshhirushan@gmail.com>
2023-01-21 20:46:59 +0530

    Update productRoutes.js

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -1,45 +1,91 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/product');
 
 router.get('/',(req,res,next) => {
-    res.status(200).json({
-        message : "Get all products"
+    Product.find()
+    .exec()
+    .then((doc) => {
+        console.log(doc);
+        if(doc){
+            res.status(200).json(doc);
+        }
+        else{
+            res.status(404).json({message:'No valid entry found'});
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error:err});
     });
 });
 
 router.post('/',(req,res,next) => {
-    res.status(200).json({
-        message : "POST product"
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId, 
+        name : req.body.name,
+        price:req.body.price
     });
+    product.save().then(result=>{
+        console.log('result');
+        res.status(200).json({
+            message : "POST product",
+            content:product
+        });
+    })
+    .catch(err => console.log(err));
 });
 
 router.get('/:productId',(req,res,next) => {
-    const Id = req.params.productId;
-    res.status(200).json({
-        message : "GET product by productId",
-        content : {
-            productId : Id
+    const id = req.params.productId;
+    Product.findById(id)
+    .exec()
+    .then((doc) => {
+        console.log(doc);
+        if(doc){
+            res.status(200).json(doc);
         }
+        else{
+            res.status(404).json({message:'No valid entry found for provided id'});
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error:err});
     });
 });
 
 router.patch('/:productId',(req,res,next) => {
-    const Id = req.params.productId;
-    res.status(200).json({
-        message : "Update product by productId",
-        content : {
-            productId : Id
-        }
+    const id = req.params.productId;
+    const updateOps = {};
+    for(const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+    console.log(updateOps);
+    Product.updateOne({_id:id},{$set:updateOps})
+    .exec()
+    .then((result) => {
+        //console.log(result);
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error:err});
     });
 });
 
 router.delete('/:productId',(req,res,next) => {
-    const Id = req.params.productId;
-    res.status(200).json({
-        message : "DELETE product by productId",
-        content : {
-            productId : Id
-        }
+    const id = req.params.productId;
+    Product.remove({_id:id})
+    .exec()
+    .then((doc) => {
+        console.log(doc);
+        res.status(200).json(doc);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error:err});
     });
 });
 
