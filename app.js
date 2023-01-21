f5ee96a8648883af5b67211ba911e5c81ed5ed25
Hirushan-N <nadeeshhirushan@gmail.com>
2023-01-21 14:35:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require('express');
-
 const app = express();
 
-app.use((req,res,next)=>{
-    res.status(200).json({
-        message : "Successful"
-    });
-})
+const productRoutes = require('./api/routes/productRoutes')
+
+app.use('/products',productRoutes);
 
 module.exports = app;
