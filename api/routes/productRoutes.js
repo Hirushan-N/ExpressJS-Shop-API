c3899277f0bcc6478fc338678c8ac94159e1126d
Hirushan-N <nadeeshhirushan@gmail.com>
2023-01-22 12:22:18 +0530

    Update productRoutes.js

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -3,90 +3,115 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Product = require('../models/product');
 
-router.get('/',(req,res,next) => {
+router.get('/', (req, res, next) => {
     Product.find()
-    .exec()
-    .then((doc) => {
-        console.log(doc);
-        if(doc){
-            res.status(200).json(doc);
-        }
-        else{
-            res.status(404).json({message:'No valid entry found'});
-        }
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({error:err});
-    });
+        .exec()
+        .then((doc) => {
+            console.log(doc);
+            if (doc) {
+                res.status(200).json(doc);
+            }
+            else {
+                res.status(404).json({ message: 'No valid entry found' });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
 });
 
-router.post('/',(req,res,next) => {
+router.post('/', (req, res, next) => {
     const product = new Product({
-        _id: new mongoose.Types.ObjectId, 
-        name : req.body.name,
-        price:req.body.price
+        _id: new mongoose.Types.ObjectId,
+        name: req.body.name,
+        price: req.body.price
     });
-    product.save().then(result=>{
+    product.save().then(result => {
         console.log('result');
         res.status(200).json({
-            message : "POST product",
-            content:product
+            message: "POST product",
+            content: product
         });
     })
-    .catch(err => console.log(err));
+        .catch(err => console.log(err));
 });
 
-router.get('/:productId',(req,res,next) => {
+router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
-    .exec()
-    .then((doc) => {
-        console.log(doc);
-        if(doc){
+        .exec()
+        .then((doc) => {
+            console.log(doc);
+            if (doc) {
+                res.status(200).json(doc);
+            }
+            else {
+                res.status(404).json({ message: 'No valid entry found for provided id' });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
+});
+
+router.patch('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    //#region New after 2021
+    Product.findByIdAndUpdate(id, { $set: req.body }, { new: true })
+        .exec()
+        .then((result) => {
+            //console.log(result);
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            //console.log(err);
+            res.status(500).json({ error: err });
+        });
+    //#endregion
+
+    /* SAMPLE req FOR POSTMAN TESTING
+    [
+        {
+            "propName": "name",
+            "value": "Single Rool Book (CR)"
+        }
+    ]
+    */
+
+    //#region BEFORE 2021
+    // const updateOps = {};
+    // for(const ops of req.body){
+    //     updateOps[ops.propName] = ops.value;
+    // }
+    // //console.log(updateOps);
+    // Product.updateOne({_id:id},{$set:updateOps})
+    // .exec()
+    // .then((result) => {
+    //     //console.log(result);
+    //     res.status(200).json(result);
+    // })
+    // .catch(err => {
+    //     //console.log(err);
+    //     res.status(500).json({error:err});
+    // });
+    //#endregion
+
+});
+
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    Product.findByIdAndRemove(id) // can use Product.remove({ _id: id }) instead(cannot return the deleted object.only returns a report)
+        .exec()
+        .then((doc) => {
+            console.log(doc);
             res.status(200).json(doc);
-        }
-        else{
-            res.status(404).json({message:'No valid entry found for provided id'});
-        }
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({error:err});
-    });
-});
-
-router.patch('/:productId',(req,res,next) => {
-    const id = req.params.productId;
-    const updateOps = {};
-    for(const ops of req.body){
-        updateOps[ops.propName] = ops.value;
-    }
-    console.log(updateOps);
-    Product.updateOne({_id:id},{$set:updateOps})
-    .exec()
-    .then((result) => {
-        //console.log(result);
-        res.status(200).json(result);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({error:err});
-    });
-});
-
-router.delete('/:productId',(req,res,next) => {
-    const id = req.params.productId;
-    Product.remove({_id:id})
-    .exec()
-    .then((doc) => {
-        console.log(doc);
-        res.status(200).json(doc);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({error:err});
-    });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
 });
 
 
